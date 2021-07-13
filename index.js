2d50adaf31a1e3bc89d6d8277d900d1b42807a11
Samuel Dourado <samuel.dourado7038@gmail.com>
2021-07-13 01:41:32 -0300

    feat: Create users route file.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express, { response } from 'express'
 import bodyParser from 'body-parser'
+import usersRoutes from './routes/users.js'
 
 const app = express()
 const PORT = 5000
 
 app.use(bodyParser.json())
 
+app.use('/users', usersRoutes)
+
+// Creating new route. 
 app.get('/', (request, response) => {
-    console.log('[Teste]')
     response.send('Hello from HomePage!')
 })
 
