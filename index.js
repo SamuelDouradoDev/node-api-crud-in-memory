b7adced9d2ebf812f00c7e768182a8ddd9ab94d1
Samuel Dourado <samuel.dourado7038@gmail.com>
2021-07-13 01:29:52 -0300

    feat: add new route.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import express from 'express'
+import express, { response } from 'express'
 import bodyParser from 'body-parser'
 
 const app = express()
 const PORT = 5000
 
 app.use(bodyParser.json())
+
+app.get('/', (request, response) => {
+    console.log('[Teste]')
+    response.send('Hello from HomePage!')
+})
+
 app.listen(PORT, () => console.log(`Server runing on http://localhost:${PORT}`))
