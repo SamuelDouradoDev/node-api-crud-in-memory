5610c829714d749e6648542e5af579cfdfe69407
Samuel Dourado <samuel.dourado7038@gmail.com>
2021-07-13 01:51:15 -0300

    feat: Create  get/users route.

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,22 @@ import express, { request, response } from 'express'
 
 const router = express.Router()
 
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    }, 
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+];
+
 router.get('/', (request, response) => {
-    response.send('Hello')
+    console.log(users)
+    response.send(users)
 })
 
 export default router
