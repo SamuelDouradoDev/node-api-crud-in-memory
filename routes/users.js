0c621a393aa50b29bb9d2486359cfb04ad134414
Samuel Dourado <samuel.dourado7038@gmail.com>
2021-07-13 23:25:07 -0300

    feat: Add ID to the user.

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,17 @@
 import express, { request, response } from 'express'
+import { v4 as uuidv4} from 'uuid'
 
 const router = express.Router()
 
 const users = [
     {
+        id: uuidv4(),
         firstName: "John",
         lastName: "Doe",
         age: 25
     }, 
     {
+        id: uuidv4(),
         firstName: "Jane",
         lastName: "Doe",
         age: 24
@@ -22,7 +25,7 @@ router.get('/', (request, response) => {
 
 router.post('/', (request, response) => {
     const user = request.body
-    users.push(user)
+    users.push({ ...user, id: uuidv4() })
     response.send(`User with the name ${user.firstName} added to the database!`)
 })
 
